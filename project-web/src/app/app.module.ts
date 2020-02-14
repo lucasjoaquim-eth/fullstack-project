fdd466b7a30b7740876dad5baa63643d8765efe9
Lucas Joaquim da Silva <lucasjoaquim147@gmail.com>
2020-02-14 16:40:41 -0300

    Includ Nav-Bar

--- project-web/src/app/app.module.ts
+++ project-web/src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventComponent } from './pages/event/event.component';
+import { NavComponent } from './pages/nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, EventComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [AppComponent, EventComponent, NavComponent],
+  exports: [],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CommonModule, MatToolbarModule,
+  BrowserAnimationsModule,MatDividerModule,MatCardModule
+  ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}

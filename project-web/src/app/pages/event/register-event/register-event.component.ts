bec30a71fba32147175f254db771dfb04504c7f8
Lucas Joaquim da Silva <lucasjoaquim147@gmail.com>
2020-02-29 08:27:09 -0300

    Organize - Register

---
+++ project-web/src/app/pages/event/register-event/register-event.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from "@angular/core";
+import { Validators, FormGroup, FormBuilder } from "@angular/forms";
+
+@Component({
+  selector: "app-register-event",
+  templateUrl: "./register-event.component.html",
+  styleUrls: ["./register-event.component.css"]
+})
+export class RegisterEventComponent implements OnInit {
+  imagemUrl: string;
+  place: string;
+  theme: string;
+  email: string;
+  phone: string;
+  date: Date;
+  amountPeople: Number;
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.validation();
+  }
+
+  validation() {
+    this.registerForm = this.formBuilder.group({
+      imagemUrl: ["", [Validators.required]],
+      date: ["", [Validators.required]],
+      place: ["", [Validators.required]],
+      amountPeople: ["", [Validators.required, Validators.maxLength(1200)]],
+      phone: ["", [Validators.required]],
+      theme: [
+        "",
+        [Validators.required, Validators.minLength(3), Validators.maxLength(50)]
+      ],
+      email: ["", [Validators.required, Validators.email]]
+    });
+  }
+  saveChange() {}
+  close() {}
+}

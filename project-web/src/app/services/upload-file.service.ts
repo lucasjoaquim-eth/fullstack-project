fe50ce44adb55d864a1dc021d0255539f2c9462d
Lucas Joaquim da Silva <lucasjoaquim147@gmail.com>
2020-03-16 23:51:19 -0300

    Improve: Include Upload File Service

---
+++ project-web/src/app/services/upload-file.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpEvent, HttpErrorResponse, HttpEventType } from  '@angular/common/http';  
+import { map } from  'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UploadFileService {
+  SERVER_URL: string = "https://file.io/";
+  constructor(private httpClient: HttpClient) { }
+  public upload(formData) {
+    return this.httpClient.post<any>(this.SERVER_URL, formData, {  
+        reportProgress: true,  
+        observe: 'events'  
+    });  
+  }
+}
